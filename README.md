ae65df5b4bcca7d904def397a2cf5f45cde697d2
David <dmrubins@gmail.com>
2017-09-21 09:17:11 -0400

    Updated for HMS

--- README.md
+++ README.md
@@ -1,42 +1,28 @@
-# WashU Library Chrome Extension
+# HMS Library Chrome Extension
 
-Install the extension for Chrome at [the Chrome Web Store](https://chrome.google.com/webstore/detail/amamhphjnhlgdpjhfmnkikcingjfoeao).
+Install the extension for Chrome at [the Chrome Web Store](https://chrome.google.com/webstore/detail/auto-library-proxy-access/bdniimphleccnkgjachlpokfeljghpgm).
 
 ## Overview
 
 This is a Chrome extension that automatically rewrites journal URLs to include
-the WashU library proxies (both Danforth and Becker proxies).
+the HMS library proxy.
 
-These proxies are:
-
- * https://beckerproxy.wustl.edu
- * https://login.libproxy.wustl.edu
-
-e.g. nature.com would become: nature.com.beckerproxy.wustl.edu OR nature.com.libproxy.wustl.edu
-
-
-NOTE: This extension requires either a WUSTL Key or a Becker Proxy account.
+NOTE: This extension requires a HarvardKey.
 
 ## Options
 
  * Auto Redirect (default: true)
   * Should we automatically redirect the page if it's in a database of known journals ([LIB]-journals.js)?
- * Enable Danforth Proxy (default: true)
-  * Should we use libproxy.wustl.edu?
- * Enable Becker Proxy (default: true)
-  * Should we use beckerproxy.wustl.edu (or beckerproxy2a.wucon.wustl.edu)?
- * Prefer Danforth Proxy (default: false)
-  * If a journal is in both proxies (e.g. Nature) and both are enabled, should we prefer Danforth?
-
 
 ## Authors
 **Nick Semenkovich**
+**David Rubins**
 
 + https://github.com/semenko/
 + https://nick.semenkovich.com/
 
 ## License
-Copyright 2011-2015, Nick Semenkovich <semenko@alum.mit.edu>
+Copyright 2011-2017, Nick Semenkovich <semenko@alum.mit.edu> and David Rubins
 
 Released under the MIT License. See LICENSE for details.
 
--- javascript/options.js
+++ javascript/options.js
@@ -13,90 +13,20 @@ function $(id) {
 //adding listener when body is loaded to call init function.
 window.addEventListener('load', init, false);
 
-function updateStatusBar() {
-    // Update the status bar with the current network
-    chrome.extension.sendRequest({operation: "getstate"}, function(response) {
-        if (response.networkTag == "OFF") {
-            $('statusBar').innerText = '(Current network: Off-Campus';
-            $('statusBar').innerHTML += ' - <a href="#" id="redetect" style="color:#888">Redetect Now</a> )';
-        } else {
-            $('statusBar').innerText = '(Current network: ' + response.networkTag;
-            $('statusBar').innerHTML += ' - <a href="#" id="redetect" style="color:#888">Redetect Now</a> )';
-        }
-	document.getElementById('redetect').addEventListener('click', function(){
-		detectNow();
-	    }, false);
-
-    });
-}
-
 // Set values based on localStorage
 function init() {
     parseLocalStorage();
     $('autoRedirect').checked = optAutoRedirect;
-    $('enableDanforth').checked = optEnableDanforth;
-    $('enableBecker').checked = optEnableBecker;
     $('preferDanforth').checked = optPreferDanforth;
-    $('usageOptOut').checked = optUsageOptOut;
-    buttonLogic();
-    // Show user their detected network
-    updateStatusBar();
-}
-
-function detectNow() {
-    // Redetect network
-    chrome.extension.sendRequest({operation: "redetect"}, function(response) {});
-    // The network state request is async, and sometimes returns in a few seconds.
-    // TODO: Clean this up by making the request optionally synchronous.
-    updateStatusBar();
-    setTimeout(function(){ updateStatusBar(); }, 1000);
-    setTimeout(function(){ updateStatusBar(); }, 2000);
-    setTimeout(function(){ updateStatusBar(); }, 3000);
-    setTimeout(function(){ updateStatusBar(); }, 5000);
-}
-
-function buttonLogic() {
-    // Simple logic to force at least one proxy to be enabled
-    if (!$('enableDanforth').checked) {
-        $('enableBecker').checked = true;
-        $('enableBecker').disabled = true;
-        $('preferDanforth').checked = false;
-        $('preferDanforth').disabled = true;
-    } else {
-        $('enableBecker').disabled = false;
-    }
-
-    if (!$('enableBecker').checked) {
-        $('enableDanforth').checked = true;
-        $('enableDanforth').disabled = true;
-        $('preferDanforth').checked = false;
-        $('preferDanforth').disabled = true;
-    } else {
-        $('enableDanforth').disabled = false;
-    }
-
-    // If both are enabled, allow preference
-    if ($('enableDanforth').checked && $('enableBecker').checked) {
-        $('preferDanforth').disabled = false;
-    }
-
-    // I don't think it's possible to get here, but just in case ...
-    if (!$('enableDanforth').checked && !$('enableBecker').checked) {
-        $('enableDanforth').checked = true;
-        $('enableDanforth').disabled = false;
-    }
+    //$('usageOptOut').checked = optUsageOptOut;
 }
 
 /**
  * Saves the value of the checkbox into local storage.
  */
 function save() {
-    buttonLogic();
     localStorage.autoRedirect = $('autoRedirect').checked;
-    localStorage.enableDanforth = $('enableDanforth').checked;
-    localStorage.enableBecker = $('enableBecker').checked;
-    localStorage.preferDanforth = $('preferDanforth').checked;
-    localStorage.usageOptOut = $('usageOptOut').checked;
+    //localStorage.usageOptOut = $('usageOptOut').checked;
     // Make sure the background page sees the changes!
     chrome.extension.getBackgroundPage().parseLocalStorage();
     $('autoRedirectStatus').innerHTML = 'Saved.';
